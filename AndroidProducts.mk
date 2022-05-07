45eead2d4128f991e4471df5d6934e018a71a9bd
ymdzq <1422091+ymdzq@users.noreply.github.com>
2022-05-07 19:06:21 +0800

    配置文件描述统一改为twrp格式

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_elish.mk
+    $(LOCAL_DIR)/twrp_elish.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_elish-user \
-    omni_elish-userdebug \
-    omni_elish-eng
+    twrp_elish-user \
+    twrp_elish-userdebug \
+    twrp_elish-eng
--- twrp_elish.mk
+++ twrp_elish.mk
@@ -13,13 +13,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from elish device
 $(call inherit-product, device/xiaomi/elish/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := elish
-PRODUCT_NAME := omni_elish
+PRODUCT_NAME := twrp_elish
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := M2105K81AC
 PRODUCT_MANUFACTURER := xiaomi
