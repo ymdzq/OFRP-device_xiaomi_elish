8660912ac4dd38f0811cdb35c97b6ae5f3078281
ymdzq <1422091+ymdzq@users.noreply.github.com>
2022-07-13 21:02:18 +0800

    加入python

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,6 +169,9 @@ TW_NO_HAPTICS := true
 TW_INCLUDE_RESETPROP := true
 TW_INCLUDE_LIBRESETPROP := true
 
+# python, for ABX xml conversion (change to true to enable)
+TW_INCLUDE_PYTHON := true
+
 ifeq ($(TW_DEVICE_VERSION),)
 TW_DEVICE_VERSION=12.0
 endif
