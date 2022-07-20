1efeed2a4cc403fb5088a1cbffbdde2a4aa93e12
ymdzq <233900743@qq.com>
2022-07-20 22:26:15 +0800

    删除libandroidicu libandroidicu由于twrp-a12已经自带了，就不需要重复写

--- device.mk
+++ device.mk
@@ -50,11 +50,7 @@ PRODUCT_PACKAGES += \
 
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
-    libandroidicu \
     libion
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
-
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
