fee3e6ae4e54b13f2eebffac3ecb1cb8b8b64bd7
ymdzq <1422091+ymdzq@users.noreply.github.com>
2022-08-02 20:18:24 +0800

    删除多余ab分区设置

--- device.mk
+++ device.mk
@@ -19,29 +19,10 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 # A/B
 ENABLE_VIRTUAL_AB := true
 
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    checkpoint_gc \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
 # Boot Control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
     android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service \
-    bootctrl.kona \
     bootctrl.kona.recovery
-	
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
 
 # QCOM Decryption
 PRODUCT_PACKAGES += \
