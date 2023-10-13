6925c064cd3c0bc92adadb02b48e8222de236657
ymdzq <1422091+ymdzq@users.noreply.github.com>
2023-10-13 20:54:30 +0800

    删除跳过新版fstab处理的标记 标记已经改名OF_FORCE_USE_RECOVERY_FSTAB 而这个设备不需要

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -117,8 +117,6 @@
 	# export FOX_VENDOR_BOOT_RECOVERY=1
 	# 设置默认时区为北京时间
 	export OF_DEFAULT_TIMEZONE="TAIST-8;TAIDT"
-	# 跳过新版fstab处理
-	# export OF_LEGACY_PROCESS_FSTAB=1
 
 	F=$(find "device" -maxdepth 2 -name "elish")
 	# 修改启动画面背景色为#000000
