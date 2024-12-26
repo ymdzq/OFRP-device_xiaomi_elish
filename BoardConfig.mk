a2e053e9147ef2283af4190ab2d481e09cadc96c
ymdzq <ymdzq@users.noreply.github.com>
2024-12-26 23:58:26 +0800

    更新recovery内核 提取自澎湃OS1.0.2.0正式版的boot分区 需配合新版twrp drm代码，才能点亮屏幕 https://gerrit.twrp.me/q/topic:%22drm-fix-new-topology%22

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
 ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 endif
 
 # Kernel dtb
