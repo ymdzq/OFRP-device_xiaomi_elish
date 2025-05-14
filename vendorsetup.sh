ef902fb90aeff9aa15bae7fa11e7b7a9480ec77a
ymdzq <ymdzq@users.noreply.github.com>
2025-05-14 20:38:19 +0800

    刷入橙狐安装zip卡刷包时将恢复默认设置
    最近几个版本上游代码变化比较大，保留设置更新会导致解密完成后界面卡在控制台日志无法操作
    橙狐菜单的fox插件选项里有备份设置按钮，可以提前备份，在重置之后刷入备份包恢复

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -62,7 +62,7 @@
 	# 禁用检查rom里的compatibility.zip
 	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 	# 使用zip卡刷包的方式安装橙狐后不恢复橙狐的默认设置
-	export FOX_RESET_SETTINGS=disabled
+	# export FOX_RESET_SETTINGS=disabled
 	# 删除zip包里的AromaFM（有的设备用不了）
 	export FOX_DELETE_AROMAFM=1
 
